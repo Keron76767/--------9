5a8fa439f3a66c7be04188337a2a2a2aa05f1f3d
Keron76767 <keronsilver777@gmail.com>
2024-12-11 18:19:06 +0300

    first commit

---
+++ 1/src/tovars.js
@@ -0,0 +1,41 @@
+export let tovarsObj = [
+    {
+        nameTovar: "Фитнес-браслет DEXP SB60",
+        imgSrc:"1.jpg",
+        corpus: "черный",
+        remen: "черный",
+        displaySize:"0.96\"",
+        displayType: "OLED", 
+        bluetooth: true,
+        OS:{
+            os1:"Android 4.2",
+            os2:"iOS 8",
+        }
+    },
+    {
+        nameTovar: "Смарт-часы RunGo W2",
+        imgSrc:"2.jpg",
+        corpus: "зелёный",
+        remen: "зелёный",
+        displaySize:".3\"",
+        displayType: "TFT (TN)", 
+        bluetooth: false,
+        OS:{
+            os1:"Android",
+            os2:"iOS",
+        }
+    },
+    {
+        nameTovar: "Смарт-часы Welbe Prime",
+        imgSrc:"3.jpg",
+        corpus: "чёрный",
+        remen: "розовый",
+        displaySize:"1.69\"",
+        displayType: "IPS", 
+        bluetooth: true,
+        OS:{
+            os1:"Android 5.0",
+            os2:"iOS 9",
+        }
+    }
+]
